503a3ef55ed6b6f0c1bbd28fe351d0d6521158f8
Lucas <lucaslugui1604@gmail.com>
2025-05-13 11:28:45 -0300

    adicionando slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,25 @@
-//DECLARANDO O ARRAY DE IMAGEM
+//DECLARANDO O ARRAY DE IMAGENS
 
-let imagens = [
-    'src/assets/imagem1.jpg',
-    'src/assets/imagem2.jpg',
-    'src/assets/imagem3.jpg',
-    'src/assets/imagem4.jpg',
+let imagens =[
+    'src/assets/universo.jpg',
+    'src/assets/praia.jpg',
+    'src/assets/montanhas.jpg',
 ];
+
+//DECLARANDO AS VARIAVEIS
+
+let index=0;
+let tempo=3000;
+
+//CRIANDO A FUNÇÃO SLIDESHOW
+
+function slideShow(){
+    document.getElementById("image").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("slideShow()",tempo)
+}
+slideShow();
