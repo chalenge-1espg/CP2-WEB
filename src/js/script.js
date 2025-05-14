470ed3ed264edaf217041b3113f0556668c3bb3a
Joao <joao34palmeira.28@gmail.com>
2025-05-14 09:44:35 -0300

    adicionando todas as imagens ao slide show

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,15 @@ const imagens = [
   "src/assets/imagem12.jpg",
   "src/assets/imagem2.jpeg",
   "src/assets/imagem3.jpeg",
-  "src/assets/imagem4.jpeg"
+  "src/assets/imagem1.jpg",
+  "src/assets/imagem5.jpg",
+  "src/assets/imagem6.jpg",
+  "src/assets/imagem7.jpg",
+  "src/assets/imagem10.jpg",
+  "src/assets/imagem11.jpg",
+  "src/assets/imagem8.jpg",
+  "src/assets/imagem9.jpg",
+  "src/assets/imagem4.jpg",
 ];
 
 let index = 0;
@@ -13,4 +21,4 @@ function trocarImagem() {
   slide.src = imagens[index];
 }
 
-setInterval(trocarImagem, 3000); // Troca a cada 3 segundos
+setInterval(trocarImagem, 2000); // Troca a cada 3 segundos
